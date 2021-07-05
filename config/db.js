d00431a1bf2acaa3b35e25c0cc5859186d01c1ba
kushwahavikas045 <vikas9106931320@gmail.com>
2021-07-05 09:57:35 +0530

    fix db bugs

--- config/db.js
+++ config/db.js
@@ -1,12 +1,28 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const connectDB = () =>{
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser :true, useCreateIndex:true,useUnifiedTopology:true, useFindAndModify : true});
-    const connection = mongoose.connection;
+// const connectDB = () =>{
+//     mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser :true, useCreateIndex:true,useUnifiedTopology:true, useFindAndModify : true});
+//     const connection = mongoose.connection;
 
-    connection.once('open', () =>{
-        console.log('Database connection');
-    })
-}
+//     connection.once('open', () =>{
+//         console.log('Database connection');
+//     })
+// }
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
+
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
+};
 module.exports = connectDB;
