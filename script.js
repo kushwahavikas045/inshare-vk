f5f5a2c3ee2a4b1c8927846662b871679d560991
kushwahavikas045 <vikas9106931320@gmail.com>
2021-07-05 20:40:46 +0530

    add script file bugs

---
+++ script.js
@@ -0,0 +1,25 @@
+const File = require('./models/file');
+const fs = require('fs');
+const connectDB = require('./config/db');
+connectDB();
+async function deleteData(){
+    const pastDate = Date.now((Date.now() - 24*60*60*1000))
+    const files = await File.find({createdAt: { $lt: pastDate }});
+    if(files.length){
+        for(const file of files){
+          try {
+            fs.unlinkSync(file.path);
+            await file.remove();
+            console.log(`successful deleted ${file.filename}`);
+          } catch (error) {
+              console.log(`Error while delete file ${error}`);
+          }
+        }
+    }
+}
+
+deleteData().then(() =>{
+    console.log('All done!');
+    process.exit();
+   
+})
